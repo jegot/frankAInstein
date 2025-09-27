c9998400b77e380ece2b06485077770598b7dc58
jessica <101846013+jegot@users.noreply.github.com>
2025-09-27 10:49:34 -0400

    Update model.py
    defined encoder and diffuser to be used

--- src/model.py
+++ src/model.py
@@ -1,2 +1,14 @@
-#we could do a full pre-trained model (distilled bc I don't have computing power)
-#or like a simple nn encoder from scratch
+#meant to be ran in Colab bc of cuda processing. 
+
+#loading standard diffuser model
+pipe = StableDiffusionImg2ImgPipeline.from_pretrained(
+    "runwayml/stable-diffusion-v1-5",
+    torch_dtype=torch.float16,
+    scheduler=DDIMScheduler.from_pretrained("runwayml/stable-diffusion-v1-5", subfolder="scheduler")
+)
+pipe = pipe.to("cuda")
+
+
+# Load pretrained VAE
+vae = AutoencoderKL.from_pretrained("CompVis/stable-diffusion-v1-4", subfolder="vae")
+vae.eval()
